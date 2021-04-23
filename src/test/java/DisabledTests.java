024f9cba472ad307704ec692418cc50a2c3ba17e
espereverzeva <katya_novikova_1990_29@mail.ru>
2021-04-23 23:14:21 +0700

    добавлены тесты

---
+++ src/test/java/DisabledTests.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisabledTests {
+    public class DisabledTests {
+        @Test
+        @Disabled
+        void some1Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some2Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some3Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some4Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some5Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some6Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some7Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some8Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some9Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some10Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        @Disabled
+        void some11Test() {
+            assertTrue(true);
+        }
+    }
+}
---
+++ src/test/java/SomeMorePositiveTests.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeMorePositiveTests {
+    public class SomeMorePositiveTests {
+        @Test
+        void some1Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some2Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some3Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some4Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some5Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some6Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some7Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some8Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some9Test() {
+            assertTrue(true);
+        }
+}
